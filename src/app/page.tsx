b863922088a520c047d7c7ee4704648d27a42134
Shivam Kumar <shivam.chandrayan@gmail.com>
2024-11-14 10:14:16 +0530

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <div className="hero-bg-graphic px-20">
+        <img src="/hero-bg-graphic.png" alt="" className="w-full" />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="">
+    <nav className="sticky">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
